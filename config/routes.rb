554e6bbf561f1addda82497d9ab07fceb7f14d63
Tery Lim <kumabotz@gmail.com>
2014-07-12 01:36:27 +0800

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Pop::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
   root 'pop#index'
 
   # Example of regular route:

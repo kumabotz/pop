20d79812d21e0283c5423ffb3aecbf5405125b6c
Tery Lim <kumabotz@gmail.com>
2014-07-12 15:17:11 +0800

    Add fonts to assets paths

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Pop::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Ignore bad email addresses and do not raise email delivery errors.
